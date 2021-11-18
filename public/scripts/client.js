4afb4b39922c0a280aa53599b57ea249f634d2ee
Andrew <andrew.spon@gmail.com>
2021-11-18 01:38:54 +0000

    fix order of tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 //////TEMP DATA//////////
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 
 
@@ -39,7 +39,7 @@ $(document).ready(function () {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
-      $("#tweets-container").append($tweet);
+      $("#tweets-container").prepend($tweet);
     }
   };
 
@@ -68,6 +68,48 @@ $(document).ready(function () {
     
   };
 
-  renderTweets(data);
+  // renderTweets(data);
+
+  const loadTweets = (tweets) => {
+    $.ajax("/tweets", { method: "GET" })
+    .then(function(tweets) { 
+      renderTweets(tweets);
+      console.log("success", tweets)
+    })
+  };
+  loadTweets();
+
+  $("#post-tweet").submit(function(event){
+    // Prevent change of page to /tweets
+    event.preventDefault();
+    // AJAX Request w/ POST method
+    // would but put IF here
+    // console.log(this)
+    if (dataVal($("#tweet-text"))) {
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $(this).serialize()
+      })
+      .then (function() { 
+        loadTweets()
+      })
+    } 
+    
+  });
+
+  const dataVal = (data) => {
+    console.log(data)
+    if (parseInt($(".counter").val()) <= 0) {
+      alert("too many characters");
+      return false;
+    } else if (parseInt($(".counter").val()) === 140) {
+      alert("Message is empty");
+      return false;
+    }
+    return true;
+  }
 
 });
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,9 @@
-const tweetText = document.getElementById("tweet-text");
-const counter = document.getElementsByName("counter");
-
-
-$(tweetText).on("input", function() {
+$("#tweet-text").on("input", function() {
   const count = 140;// $(counter).val
   const tweetCount =  $(this).val().length;
-  $(counter).text(count-tweetCount);
-  if($(counter).val() < 0) {
-    $(counter).css("color", "red")
+  $(".counter").text(count-tweetCount);
+  if($(".counter").val() < 0) {
+    $(".counter").css("color", "red")
   } else {
-    $(counter).css("color", "black")}
+    $(".counter").css("color", "black")}
 })
-
