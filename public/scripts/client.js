5effc9ebcabf14bbe22ff50416b3920117a6e830
Andrew <andrew.spon@gmail.com>
2021-11-19 20:32:46 +0000

    clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-//////TEMP DATA//////////
+
 $(document).ready(function () {
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-
-
   const renderTweets = (tweets) => {
     // loops through tweets
     for (let tweet of tweets) {
@@ -94,6 +66,7 @@ $(document).ready(function () {
         url: "/tweets",
         data: $(this).serialize()
       })
+      //refresh on submission
       .then (function() { 
         $(".new-tweet-error").hide();
         $("#tweet-form-text").val("");
@@ -104,6 +77,7 @@ $(document).ready(function () {
     
   });
 
+  //check tweets for criteria
   const isDataValid = (data) => {
     if (data.val().length > 140) {
       $(".new-tweet-error").text("This tweet has gone beyond the character limit.").show();
