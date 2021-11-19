62f76592da1c17b806fc69f53e1bea2a8e94bb8f
Andrew <andrew.spon@gmail.com>
2021-11-19 02:25:48 +0000

    change error alert w/ JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -95,6 +95,8 @@ $(document).ready(function () {
         data: $(this).serialize()
       })
       .then (function() { 
+        $(".new-tweet-error").hide();
+        $("#tweet-form-text").val("");
         loadTweets()
       })
     } 
@@ -103,17 +105,15 @@ $(document).ready(function () {
 
   const isDataValid = (data) => {
     if (data.val().length > 140) {
-      alert("too many characters");
+      $(".new-tweet-error").text("This tweet has gone beyond the character limit.").show();
       return false;
     } else if (data.val().length === 0) {
-      alert("Message is empty");
+      $(".new-tweet-error").text("This tweet is empty.").show();
       return false;
     }
     return true;
   }
 
 });
-
-
 
 
